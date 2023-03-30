b4d073fa0ecc36e69763f9bfbf80c03101e12432
Maxim Smolyakov <maxmerklol@gmail.com>
2023-03-30 17:01:49 +0800

    Update Gradle related files and Android manifests

--- features/settings/build.gradle.kts
+++ features/settings/build.gradle.kts
@@ -8,7 +8,6 @@ android {
 
     defaultConfig {
         minSdk = 23
-        targetSdk = 32
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -30,13 +29,14 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    namespace = "com.maximapps.settings"
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
