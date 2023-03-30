f1c272cbfd0b748eaedc4362d751653fbaed4edd
Maxim Smolyakov <maxmerklol@gmail.com>
2023-03-30 17:23:08 +0800

    Update MainActivity.kt

--- app/src/main/java/com/maximapps/pine/MainActivity.kt
+++ app/src/main/java/com/maximapps/pine/MainActivity.kt
@@ -3,40 +3,24 @@ package com.maximapps.pine
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.maximapps.pine.ui.theme.PineTheme
+import com.maximapps.coreui.theme.PineTheme
+import com.maximapps.pine.navigation.MainScreenDestinationFactory
+import com.maximapps.pine.navigation.SettingsScreenDestinationFactory
+import com.madfrog.navigation.accompanist.AccompanistNavHost
+import com.maximapps.pine.navigation.Destinations
 
 class MainActivity : ComponentActivity() {
+    private val factories = setOf(
+        MainScreenDestinationFactory(),
+        SettingsScreenDestinationFactory()
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PineTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting()
-                }
+                AccompanistNavHost(Destinations.mainScreen, factories)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting() {
-    Text(text = "Pine - Focus Timer")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PineTheme {
-        Greeting()
     }
 }
