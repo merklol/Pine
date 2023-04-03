4a503102056a74c18f7c704a2328b140069f6f0c
Maxim Smolyakov <maxmerklol@gmail.com>
2023-04-03 14:31:39 +0800

    Remove the sampple app

--- app/src/main/java/com/maximapps/pine/navigation/SettingsScreenTransitions.kt
+++
@@ -1,7 +0,0 @@
-package com.maximapps.pine.navigation
-
-import com.madfrog.navigation.accompanist.AccompanistTransitions
-import com.madfrog.navigation.accompanist.slideInTransition
-import com.madfrog.navigation.accompanist.slideOutTransition
-
-class SettingsScreenTransitions : AccompanistTransitions(slideInTransition(), slideOutTransition())
--- features/main/src/main/java/com/maximapps/main/utils/Visibility.kt
+++
@@ -1,6 +0,0 @@
-package com.maximapps.main.utils
-
-object Visibility {
-    const val Visible = 1f
-    const val Invisible = 0f
-}
