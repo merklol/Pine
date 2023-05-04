9ca49b9ce20d75550248a5cb1c55c27e192b9c59
Maxim Smolyakov <maxmerklol@gmail.com>
2023-05-04 14:32:07 +0800

    Remove extra space in ViewModelKey.kt

--- core/src/main/java/com/madfrog/core/di/ViewModelKey.kt
+++ core/src/main/java/com/madfrog/core/di/ViewModelKey.kt
@@ -5,7 +5,7 @@ import dagger.MapKey
 import kotlin.reflect.KClass
 
 /**
- * Annotation to define  key for Dagger bound ViewModels.
+ * Annotation to define key for Dagger bound ViewModels.
  */
 @[MapKey Target(AnnotationTarget.FUNCTION)]
 annotation class ViewModelKey(val value: KClass<out ViewModel>)
